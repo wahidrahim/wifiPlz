6ceca83eaf7724083e3af3169e30be081183745f
Wahid Rahim <wahidrahim@gmail.com>
2016-03-08 16:01:43 -0500

    heroku ready

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mainCtrl = require('../controllers/main');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', mainCtrl.index);
 
 module.exports = router;
